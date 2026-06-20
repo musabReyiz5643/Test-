9b41619a7f3e64284fac112f3bb04a9025397bf5
musabReyiz5643 <akgunmusab3@gmail.com>
2026-06-20 12:43:40 +0300

    Controllin_system'in test dosyası yazıldı

--- test/controllig_system.py
+++ test/controllig_system.py
@@ -1,7 +1,11 @@
 def controlling_system(no, status):
-    if no == 4 and status == "string-4-status-error":
+    if no == 4 or status == "string-4-status-error":
         print("this can be create a high problem in system !")
+    elif no == 5 or status == "string-5-status-error":
+        print("this can be create a middle problem in system !")
     else:
         print("there is no problem in the system")
-        
-controlling_system(4 , "string-4-status-error")
+
+controlling_system(4, "string-4-status-error") # Testing 
+controlling_system(5, "string-4-status-error") # Testing-2
+controlling_system(6, "string-6-status-error") # Testing-3 
