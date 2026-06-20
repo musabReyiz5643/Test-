32525c4d195af9fb1204eaf6e3e66f2633bf35cd
musabakgun <akgunmusab3@gmail.com>
2026-06-20 11:56:50 +0300

    App.tsx dosyasına system objesi eklendi

--- test/app.tsx
+++ test/app.tsx
@@ -3,9 +3,17 @@ function system(no: number, status: number | string) {
   const system_message = (no: number, status: number | string) =>
     `${no} and ${status}`;
 
-  if (no === 4 && status === "string-5-status-error") {
-    return system_message(no, status);
+  if (no === 4 && status === "string-4-status-error") {
+    return (obj = {
+      hata: system_message(no, status) + "% problem ihtimali orta",
+    });
+  } else if (no === 5 && status === "string-5-status-error") {
+    return (obj = {
+      hata: system_message(no, status) + "% problem ihtimali yüksek",
+    });
   } else {
-    return system_message(no, status);
+    return (obj = {
+      hata: system_message(no, status) + "% problem ihtimali yok",
+    });
   }
 }
