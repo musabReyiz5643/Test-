c3d4658470c5dae2a4747969e29f7d527612a0de
musabakgun <akgunmusab3@gmail.com>
2026-06-20 12:02:09 +0300

    app.tsx system objesi komple silindi (sıfırdan başlanıcak)

--- test/app.tsx
+++ test/app.tsx
@@ -1,19 +1,10 @@
 function system(no: number, status: number | string) {
-  let obj = {};
   const system_message = (no: number, status: number | string) =>
     `${no} and ${status}`;
 
-  if (no === 4 && status === "string-4-status-error") {
-    return (obj = {
-      hata: system_message(no, status) + "% problem ihtimali orta",
-    });
-  } else if (no === 5 && status === "string-5-status-error") {
-    return (obj = {
-      hata: system_message(no, status) + "% problem ihtimali yüksek",
-    });
+  if (no === 4 && status === "string-5-status-error") {
+    return system_message(no, status);
   } else {
-    return (obj = {
-      hata: system_message(no, status) + "% problem ihtimali yok",
-    });
+    return system_message(no, status);
   }
 }
