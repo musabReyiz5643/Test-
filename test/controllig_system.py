dc5dba117c6eb1a22170b8a3fa3e4154553c8687
musabReyiz5643 <akgunmusab3@gmail.com>
2026-06-20 12:48:32 +0300

    controlling system hazır

--- test/controllig_system.py
+++ test/controllig_system.py
@@ -1,7 +1,7 @@
 def controlling_system(no, status):
-    if no == 4 and status == "string-4-status-error":
+    if no == 4 or status == "string-4-status-error":
         print("this can be create a high problem in system !")
+    elif no == 5 or status == "string-5-status-error":
+        print("this can be create a middle problem in system !")
     else:
         print("there is no problem in the system")
-        
-controlling_system(4 , "string-4-status-error")
